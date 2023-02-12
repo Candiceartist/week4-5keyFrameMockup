c5a946d1020c52dab56808da2e0a767df09ad830
Candice Jovon Juarez <candice.artist@gmail.com>
2023-02-11 23:32:25 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const marioJump = () => {
+    let nowJump = document.querySelector('.mario')
+    nowJump.classList.toggle('jump')
+}
+
+const marioDrive = () => {
+    let nowDrive = document.querySelector('.mario')
+    nowDrive.classList.toggle('drive')
+}
+
+const marioSpin = () => {
+    let nowSpin = document.querySelector('.mario')
+    nowSpin.classList.toggle('spin')
+}
+
+const marioShake = () => {
+    let nowShake = document.querySelector('.mario')
+    nowShake.classList.toggle('shake')
+}
+
+const marioWave = () => {
+    let nowWave = document.querySelector('.mario')
+    nowWave.classList.toggle('wave')
+}
